5f3e0232d929d44aba0e3b6c812c6c01cc031ffe
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-02-21 21:24:02 +0300

    error page style

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import batman from '../../images/batman.png';
+
+const centerh2 = {
+  textAlign:'center',
+  marginTop: '50px'
+}
+
+const centerimg = {
+  display:'block',
+  margin : '40px auto'
+}
 
 const Error = () => {
   return (
     <div className='quiz-bg'>
         <div className='container'>
-            ErrorPage
+            <h2 style={centerh2}>404 Not Found</h2>
+            <img style={centerimg} src={batman}  alt="error page"/>
         </div>
     </div>
   )
