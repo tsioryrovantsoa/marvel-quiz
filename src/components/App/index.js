617238f2ab49550b6ffc080a6c0b492663078399
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-02-21 16:59:18 +0300

    Header Landing Footer components

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+import Footer from '../Footer';
+import Header from '../Header';
+import Landing from '../Landing';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Landing />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer-container'>
+        <p>Projet realise par Tsiory - 2023 - du cours ReactJS de DonkeyGeek</p>
+        <p>Les icones Wolverine, Iron-man, Spider-man, Batman sont prises sur iconFinder.com</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
