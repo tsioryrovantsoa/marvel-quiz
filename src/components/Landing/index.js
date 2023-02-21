4ed282eea3aac9eb9c269691da65f6de7c18699a
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-02-21 19:54:16 +0300

    button and animation

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from "react";
 
 const Landing = () => {
-  return (
-    <main className='welcomePage'>
+  const [btn, setbtn] = useState(false);
+  const refwolverine = useRef(null);
+  // console.log(refwolverine);
 
-    </main>
-  )
-}
+  useEffect(() => {
+    refwolverine.current.classList.add("startingImg");
 
-export default Landing
+    setTimeout(() => {
+      refwolverine.current.classList.remove("startingImg");
+      setbtn(true);
+    }, 1000);
+  }, []);
+  //une fois executer au moment du montage uniquement
+
+  const boutton = btn && (
+    <>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </>
+  );
+
+  return <main ref={refwolverine} className="welcomePage">{boutton}</main>;
+};
+
+export default Landing;
