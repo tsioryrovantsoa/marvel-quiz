ffc92bddbe9b151d9e989763d88e2db63bbdcc36
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-02-23 11:51:36 +0300

    authorization

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,16 +1,37 @@
-import React from 'react'
-import Logout from '../Logout'
-import Quiz from '../Quiz'
+import React, { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Firebase/firebaseconfig"; 
+import Logout from "../Logout";
+import Quiz from "../Quiz";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-  return (
-    <div className='quiz-bg'>
-        <div className='container'>
-            <Logout />
-            <Quiz />
-        </div>
-    </div>
-  )
-}
+  const [usersession, setusersession] = useState(null);
 
-export default Welcome
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const listener = onAuthStateChanged(auth, user => {
+      user ? setusersession(user) : navigate('/') 
+    })
+
+    return listener();
+  }, [])
+  
+
+  return usersession == null ? (
+    <>
+      <div className="loader"></div>
+      <p className="loaderText">Loading ...</p>
+    </>
+  ) : (
+    <div className="quiz-bg">
+      <div className="container">
+        <Logout />
+        <Quiz />
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
