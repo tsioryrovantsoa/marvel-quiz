3878954c62e1f9fe7700154c45f9f135fd79a608
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-03-08 22:28:24 +0300

    tooltip

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../Firebase/firebaseconfig";
 import { useNavigate } from "react-router-dom";
+import {Tooltip}  from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 
 const Logout = () => {
@@ -34,7 +36,8 @@ const Logout = () => {
     <div className="logoutContainer">
       <label className="switch">
         <input onChange={handleChange} type="checkbox" checked={checked} />
-        <span className="slider round"></span>
+        <span className="slider round" data-tooltip-id="my-tooltip" data-tooltip-content="Deconnexion" ></span>
+        <Tooltip id="my-tooltip" place="left"/>
       </label>
     </div>
   );
